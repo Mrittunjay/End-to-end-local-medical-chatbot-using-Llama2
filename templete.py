3ac044f56df39e5a492b7ccb4370d22e0796c306
Sipan <paul.mrittunjoy777@gmail.com>
2024-08-04 13:48:18 +0530

    Added template.py to create folder structure & added code folder

---
+++ templete.py
@@ -0,0 +1,43 @@
+# This file will create the folder structure for the 
+# project deployment
+
+import os 
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s)')
+
+list_dir = [
+    "src/__init__.py",
+    "src/halper.py",
+    "src/prompt.py",
+    ".env",
+    "setup.py",
+    "app.py",
+    "store_index.py",
+    "static/.gitkeep",
+    "templates/chatbot.html"
+]
+
+# Creating all the files and folders in the above list
+for filepath in list_dir:
+    filepath = Path(filepath)
+    # seperating directories and files from the file path
+    directory, file = os.path.split(filepath)
+
+    # Creating the directory
+    if directory != "":
+        os.makedirs(directory, exist_ok=True)
+        logging.info(f"Created directory: {directory}")
+    # Creating the file
+    if not os.path.exists(filepath):
+        with open(file, 'w') as f:
+            pass
+        logging.info(f"Created empty file: {file}")
+    else:
+        logging.info(f"{file} file already exists")
+    
+
+
+
+
